2ff0c645672c62d899ac0db5e5891c69dccd924f
Varsha Varadarajan <nvvarsha2009@gmail.com>
2020-10-06 23:26:34 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -72,9 +72,9 @@ const siteConfig = {
     If you want to remove a social media icon, you can add the two // before the line
     or delete the line. */
     socialMedia: {
-        linkedin: 'https://www.linkedin.com/in/',
-        twitter: 'https://twitter.com/',
-        github: 'https://github.com/',
+        linkedin: 'https://www.linkedin.com/in/varsha-varadarajan/',
+        twitter: 'https://www.twitter.com/nvvarsha2009',
+        github: 'https://github.com/varsha-varadarajan',
         //facebook: 'https://www.facebook.com/',
         //instagram: 'https://www.instagram.com/',
         //youtube: 'https://www.youtube.com/',
